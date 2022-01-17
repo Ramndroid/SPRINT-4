387d3563b357f75bf9801d3758d973e1e6734e89
Ramon <rferrergrau@gmail.com>
2022-01-17 16:34:27 +0100

    Exercici 4

--- src/films.js
+++ src/films.js
@@ -20,7 +20,8 @@ function moviesAverageOfDirector(array, director) {
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+  let result = array.map(movie => movie.title).sort().slice(0, 20);
+  return result;  
 }
 
 // Exercise 5: Order by year, ascending
